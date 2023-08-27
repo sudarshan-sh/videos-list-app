333549f0ffb3db7f8087862e5f6febef327586cd
Sudarshan Sharma <mail2sudarshan.sharma@gmail.com>
2023-08-27 19:52:19 +0530

    channel card data after clicking on channel from sidebar

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,47 +1,63 @@
 import React from "react";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Link } from "react-router-dom";
-import { Box, Typography, CardContent, CardMedia } from "@mui/material";
-import { CheckCircle } from "@mui/icons-material";
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail }) => {
-  const {
-    id: { channelId },
-    snippet: { thumbnails, title },
-  } = channelDetail;
-
-  return (
-    <Box
-      sx={{
-        boxShadow: "none",
-        borderRadius: "20px",
-      }}
-    >
-      <Link to={`channel/${channelId}`}>
-        <CardContent
+const ChannelCard = ({ channelDetail, marginTop }) => (
+  <Box
+    sx={{
+      boxShadow: "none",
+      borderRadius: "20px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: { xs: "356px", md: "320px" },
+      height: "326px",
+      margin: "auto",
+      marginTop,
+    }}
+  >
+    <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          color: "#fff",
+        }}
+      >
+        <CardMedia
+          image={
+            channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture
+          }
+          alt={channelDetail?.snippet?.title}
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            textAlign: "center",
-            color: "#fff",
+            borderRadius: "50%",
+            height: "180px",
+            width: "180px",
+            mb: 2,
+            border: "1px solid #e3e3e3",
           }}
-        >
-          <CardMedia
-            image={thumbnails?.high?.url || demoProfilePicture}
-            alt={title}
-            sx={{
-              borderRadius: "50%",
-              width: "180px",
-              height: "180px",
-              mb: 2,
-              border: "1px solid #e3e3e3",
-            }}
+        />
+        <Typography variant="h6">
+          {channelDetail?.snippet?.title}{" "}
+          <CheckCircleIcon
+            sx={{ fontSize: "14px", color: "gray", ml: "5px" }}
           />
-        </CardContent>
-      </Link>
-    </Box>
-  );
-};
+        </Typography>
+        {channelDetail?.statistics?.subscriberCount && (
+          <Typography sx={{ fontSize: "15px", fontWeight: 500, color: "gray" }}>
+            {parseInt(
+              channelDetail?.statistics?.subscriberCount
+            ).toLocaleString("en-US")}{" "}
+            Subscribers
+          </Typography>
+        )}
+      </CardContent>
+    </Link>
+  </Box>
+);
 
 export default ChannelCard;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,7 +3,13 @@ import { Stack, Box } from "@mui/material";
 import { VideoCard, ChannelCard } from "../components";
 
 const Videos = ({ videos }) => (
-  <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+  <Stack
+    direction="row"
+    flexWrap="wrap"
+    justifyContent="start"
+    alignItems="start"
+    gap={2}
+  >
     {videos.map((singleVideo, index) => (
       <Box key={index}>
         {singleVideo.id.channelId ? (
