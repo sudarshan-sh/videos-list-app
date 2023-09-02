f99c667da1ec439da8e77f87b8e935837b72cc19
Sudarshan Sharma <mail2sudarshan.sharma@gmail.com>
2023-09-02 20:57:09 +0530

    ready for deployment

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
-import { CheckCircle } from "@mui/icons-material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
 import {
   demoThumbnailUrl,
   demoVideoUrl,
@@ -10,54 +11,48 @@ import {
   demoChannelTitle,
 } from "../utils/constants";
 
-const VideoCard = ({ video }) => {
-  const {
+const VideoCard = ({
+  video: {
     id: { videoId },
     snippet,
-  } = video;
-  // console.log("snippetttt: ", snippet);
-
-  return (
-    <Card
-      sx={{
-        width: { md: "320px", xs: "100%" },
-        boxShadow: "none",
-        borderRadius: 0,
-      }}
-    >
+  },
+}) => (
+  // size of the the card
+  <Card
+    sx={{
+      width: { xs: "100%", sm: "358px", md: "320px" },
+      boxShadow: "none",
+      borderRadius: 0,
+    }}
+  >
+    <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY`}>
+      <CardMedia
+        image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
+        alt={snippet?.title}
+        // size of the image inside the card similar to that of card
+        sx={{ width: { xs: "100%", sm: "358px" }, height: 180 }}
+      />
+    </Link>
+    <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-        <CardMedia
-          image={snippet?.thumbnails?.high?.url}
-          alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
-        />
+        <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+        </Typography>
       </Link>
-      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
-        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
-            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
-          </Typography>
-        </Link>
-        <Link
-          to={
-            snippet?.channelId
-              ? `/channel/${snippet?.channelId}`
-              : demoChannelUrl
-          }
-        >
-          <Typography
-            variant="subtitle2"
-            fontWeight="bold"
-            color="gray"
-            sx={{ display: "flex", alignItems: "center", mt: "5px" }}
-          >
-            {snippet?.channelTitle || demoChannelTitle}
-            <CheckCircle sx={{ fontWeight: 12, color: "gray", ml: "5px" }} />
-          </Typography>
-        </Link>
-      </CardContent>
-    </Card>
-  );
-};
+      <Link
+        to={
+          snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl
+        }
+      >
+        <Typography variant="subtitle2" color="gray">
+          {snippet?.channelTitle || demoChannelTitle}
+          <CheckCircleIcon
+            sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+          />
+        </Typography>
+      </Link>
+    </CardContent>
+  </Card>
+);
 
 export default VideoCard;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,25 +2,29 @@ import React from "react";
 import { Stack, Box } from "@mui/material";
 import { VideoCard, ChannelCard } from "../components";
 
-const Videos = ({ videos }) => (
-  <Stack
-    direction="row"
-    flexWrap="wrap"
-    justifyContent="start"
-    alignItems="start"
-    gap={2}
-  >
-    {videos.map((singleVideo, index) => (
-      <Box key={index}>
-        {singleVideo.id.channelId ? (
-          <ChannelCard channelDetail={singleVideo} />
-        ) : (
-          <VideoCard video={singleVideo} />
-        )}
-        {/* {singleVideo.id.channelId && <ChannelCard channelDetail={singleVideo} />} */}
-      </Box>
-    ))}
-  </Stack>
-);
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return "Loading...";
+
+  return (
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((singleVideo, index) => (
+        <Box key={index}>
+          {singleVideo.id.channelId ? (
+            <ChannelCard channelDetail={singleVideo} />
+          ) : (
+            <VideoCard video={singleVideo} />
+          )}
+          {/* {singleVideo.id.channelId && <ChannelCard channelDetail={singleVideo} />} */}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
 
 export default Videos;
